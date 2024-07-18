75c2a5d5548d19b4a2b0aaf9a9f519e04beb7416
vgolovnina <dumplingspic@gmail.com>
2024-07-18 18:21:56 +0400

    add search file method

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,27 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args) {
-        System.out.println("Введите первое число: ");
-        int firstNumber = new Scanner(System.in).nextInt();
 
-        System.out.println("Введите второе число: ");
-        int secondNumber = new Scanner(System.in).nextInt();
+    public static void main(String[] args) {
 
-        int summ = firstNumber + secondNumber;
-        int diff = firstNumber - secondNumber;
-        int multi = firstNumber * secondNumber;
-        double quotient = (double) firstNumber / secondNumber;
-
-        System.out.println("Сумма " + summ);
-        System.out.println("Разность " + diff);
-        System.out.println("Произведение " + multi);
-        System.out.println("Частное " + quotient);
+        int count = 0;
+        while (true){
+            System.out.println("Введите путь к файлу");
+            String path = new Scanner(System.in).nextLine();
+            File file  = new File(path);
+            boolean fileExist = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExist || file.isDirectory()) {
+                System.out.println("Указанного файла не существует или указанный путь является путём к папке, а не к файлу.");
+                //continue;
+            }
+            else {
+                count++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + count);}
+            //break;
+        }
 
     }
 }
